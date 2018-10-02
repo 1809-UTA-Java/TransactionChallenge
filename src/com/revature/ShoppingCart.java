e87b15b07d4b8764462d8944098c34c4a05823b4
ddavaloo24 <daria.davaloO@gmail.com>
2018-10-02 09:52:43 -0500

    First commit of runnable class

--- src/com/revature/ShoppingCart.java
+++ src/com/revature/ShoppingCart.java
@@ -1,5 +1,35 @@
 package com.revature;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 class ShoppingCart implements Runnable {
-    
+
+    String fileName = "grocerystore.txt";
+
+    @Override
+    public void run() {
+        Scanner sc = new Scanner(fileName);
+        FileWriter fw = new FileWriter(fileName, false);
+
+        ArrayList<String> myCollection = new ArrayList<>();
+
+        String firstLine = sc.nextLine();
+        myCollection.add(firstLine);
+
+
+
+        try(FileWriter fw = new FileWriter(fileName, false)) {
+            while(sc.hasNextLine()) {
+                fw.write(sc.nextLine());
+            }
+        } catch(IOException e) {
+            e.printStackTrace();
+        }
+
+        sc.close();
+    }
+
 }
