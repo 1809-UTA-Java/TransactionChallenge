890c75a5008e8a2d6d621d8163733dfcb8366e39
tim-deckert <tadeckert7@gmail.com>
2018-10-02 09:54:19 -0500

    Store added

--- src/com/revature/ShoppingCart.java
+++ src/com/revature/ShoppingCart.java
@@ -1,5 +1,8 @@
 package com.revature;
 
 class ShoppingCart implements Runnable {
-    
+    @Override
+    public void run () {
+        this.wait();
+    }
 }
---
+++ src/com/revature/Store.java
@@ -0,0 +1,11 @@
+package com.revature;
+
+import java.util.Random;
+
+class Store {
+    Random 
+    Thread cart1 = new ShoppingCart();
+    Thread cart2 = new ShoppingCart();
+
+    cart1.start();
+}
