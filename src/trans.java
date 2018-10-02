e07cdf3330b0ba70c23c3c7d7ecd42f510c6b395
Clint <clintjellesed@gmail.com>
2018-10-02 11:18:45 -0500

    Working Version.

--- src/trans.java
+++ src/trans.java
@@ -1,4 +1,6 @@
-//
+import java.io.*;
+import java.util.*;
+
 
 class transaction implements Runnable {
     public static void main(String[] args) {
@@ -16,19 +18,39 @@ class transaction implements Runnable {
         System.out.println("Starting Threads...");
         thread.start();
         thread2.start();
+
     }
 
     @Override
     public void run() {
         System.out.println("Inside : " + Thread.currentThread().getName());
-        ArrayList<String> arrayList = new ArrayList<>();
+        ArrayList<String> arrayList = new ArrayList<String>();
+        ArrayList<String> arrayWrite = new ArrayList<String>();
         String fileName = "example.txt";
         try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
             String line;
-            while ((line = br.readLine()) != null) {
+            //System.out.println(br.readLine());
+            if((line = br.readLine()) != null) {
                 arrayList.add(line);
-                line.trim();
+                //System.out.println(line);
             }
+            while ((line = br.readLine()) != null) {
+                arrayWrite.add(line);
+                //System.out.println(line);
+            }
+        }  catch (FileNotFoundException ex) {
+            ex.printStackTrace();
+            System.out.println("File not found\n");
+        }  catch (IOException ex) {
+            ex.printStackTrace();
         }
+
+        try(FileWriter fw = new FileWriter(fileName)) {
+            for(String thing : arrayWrite)
+                fw.write(thing);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+        System.out.println(arrayList);
     }
 }
