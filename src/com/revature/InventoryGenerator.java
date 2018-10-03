fe762e2f1259c09532afb537a9446a4dff00dd1c
Leonardo DeLeon <lqdeleon@gmail.com>
2018-10-02 21:23:30 -0500

    second commit

---
+++ src/com/revature/InventoryGenerator.java
@@ -0,0 +1,39 @@
+package com.revature;
+
+import java.io.*;
+import java.util.*;
+
+/**
+ * InventoryGenerator auto generates list of 
+ * "inventory items" into "inventory.txt" file 
+ * in the "junk" folder for the pupose of testing 
+ * TransactionChallenge program
+ * 
+ * To run it, type - java -cp bin com.revature.InventoryGererator
+ * 
+ * @author Leonardo De Leon
+ */
+public class InventoryGenerator {
+
+    public static void main(String[] args) {
+        InventoryGenerator ig = new InventoryGenerator();
+        ig.generateInventory();    
+    }
+
+    public void generateInventory () {
+        String fileName = "inventory";
+        File inputFile = new File("junk/"+(fileName+=".txt"));
+        try {
+            BufferedWriter bw = new BufferedWriter(new FileWriter(inputFile));
+            for (int i = 0; i < 100; i++) {
+                bw.write("abc"+i + System.getProperty("line.separator"));
+            }
+            bw.close();
+
+        } catch (IOException io) {
+            io.printStackTrace();
+        }
+    }
+}
+
+
--- src/com/revature/TransactionChallenge.java
+++ src/com/revature/TransactionChallenge.java
@@ -2,45 +2,61 @@ package com.revature;
 
 import java.io.*;
 import java.util.*;
+import java.lang.Runnable;
 
 /**
- * TransactionChallenge is an app that reads from a file 'sample-item'
+ * TransactionChallenge is an app that reads from a file 'inventory.txt'
  * that contains list of items (apple, bread, eggs, etc) and extract the
  * items using two separate threads and putting them into their own
  * container
  * 
+ * To run it, type java -cp bin com.revature.TransactionChallenge at the terminal
+ * 
  * @author Leonardo De Leon 
  */
-public class TransactionChallenge {
-    public static void main(String[] args) {
-        String fileName = "sample-items";
-        ArrayList<String> listOfItems = new ArrayList<>();
+public class TransactionChallenge implements Runnable {
 
-        // try open and read file
+    String fileName = "inventory";
+
+    public static void main(String[] args) {
+ 
+        TransactionChallenge tc = new TransactionChallenge();
+        Thread t1 = new Thread(tc, "Thread 1");  
+        Thread t2 = new Thread(tc, "Thread 2"); 
+        t1.start(); System.out.println(t1.getName());
+        t2.start(); System.out.println(t2.getName());
+    }
+
+	static synchronized void openReadAndWriteFilesContent (String fileName) throws IOException {
+        ArrayList <String> contents = new ArrayList<>();
+        File inputFile = new File("junk/"+(fileName+=".txt"));
+        File tempFile = new File("junk/my"+(fileName+=".txt"));
+		String line;
+        BufferedReader br = new BufferedReader(new FileReader(inputFile));
+        BufferedWriter bw = new BufferedWriter(new FileWriter(tempFile));
+        while ((line = br.readLine()) != null) {;
+            try {            
+                contents.add(line);  
+                bw.write("");   
+                Thread.sleep(15);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+		}
+        br.close();
+        bw.close();	
+        // show what's in threads 1 and threads 2
+        System.out.println(contents);
+        tempFile.renameTo(inputFile);
+    }
+
+    @Override
+    public void run() {
+        // try open and read from inventory list
         try {
-            // open the journal and read the contents
-            System.out.println("Reading files...");
-            listOfItems = openAndReadFilesContent(fileName);   
-            
-            for (int x=0; x<listOfItems.size(); x++)
-                System.out.println(listOfItems.get(x));
+            openReadAndWriteFilesContent(fileName);   
         } catch (IOException io) {
             io.printStackTrace();
         }
-
-        
-        
-        
-    }
-
-	static ArrayList<String> openAndReadFilesContent (String fileName) throws IOException {
-		ArrayList <String> contents = new ArrayList<>();
-		String line;
-		BufferedReader br = new BufferedReader(new FileReader("junk/"+(fileName+=".txt")));
-		while ((line = br.readLine()) != null) {;
-			contents.add(line);
-		}
-		br.close();		
-		return contents;
-	}    
+    }       
 }
